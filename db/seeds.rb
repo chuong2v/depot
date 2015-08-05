338f94e62909e6c9d72228eb2b1eaf3deb4f2e83
chuong2v <chuong.2v@gmail.com>
2015-08-05 14:29:03 +0700

    them file seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+products = [
+  {
+    title: "Beer",
+    description: "beer",
+    image_url: "http://pngimg.com/upload/beer_PNG2388.png",
+    price: 15000.0 
+  },
+  {
+    title: "Thuốc lá",
+    description: "Thuốc lá",
+    image_url: "http://vinapharm.com.vn/jscripts/tiny_mce/plugins/imagemanager/images/thuocla.jpg",
+    price: 20000.0 
+  }
+]
+
+products.each{|item| Product.create(
+  title: item[:title],
+  description: item[:description],
+  image_url: item[:image_url], 
+  price: item[:price]
+  )
+}
